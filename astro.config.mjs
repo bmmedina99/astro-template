c8e89f521a7740d8a91d077f464fc9037e819b7a
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2025-06-13 23:57:23 +0200

    feat(config): :sparkles: add site.config.ts for easy template configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 import robotsTxt from 'astro-robots-txt'
 import { defineConfig } from 'astro/config'
+import siteConfig from './site.config'
 
 export default defineConfig({
-  // Cambiar la URL con el dominio de tu sitio
-  site: 'https://localhost:5000',
+  site: siteConfig.canonical,
   integrations: [sitemap(), robotsTxt()],
   vite: {
     plugins: [tailwindcss()],
---
+++ src/site.config.ts
@@ -0,0 +1,8 @@
+export const configSite = {
+  title: 'AstroKit | Plantilla de Inicio',
+  description:
+    'Plantilla de inicio ultra-ligera para proyectos con Astro, preconfigurada con TypeScript y TailwindCSS.',
+  canonical: 'https://localhost:5000',
+  lang: 'es',
+  author: 'bmmedina99',
+}
