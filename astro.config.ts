ffd30eff62d9bad79bd010acef839509964fd899
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2026-03-18 01:40:40 +0100

    feat(fonts): add Poppins font configuration

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,6 @@
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 import robotsTxt from 'astro-robots-txt'
 import { configSite } from './src/site.config'
 
@@ -8,6 +8,15 @@ export default defineConfig({
   // -> Modifica la url del sitio en src/site.config.ts
   site: configSite.url,
   integrations: [sitemap(), robotsTxt()],
+  fonts: [
+    {
+      name: 'Poppins',
+      provider: fontProviders.fontsource(),
+      cssVariable: '--font-poppins',
+      weights: [400, 600, 700],
+      styles: ['normal'],
+    },
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
